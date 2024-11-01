4b997f68f643188620699aa7b28f05680ed96cfb
Gabriel Oporto <gabyoporto@gmail.com>
2024-11-01 10:32:39 -0300

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import MenuAndLogo from "./Header/MenuAndLogo.tsx";
 import Cart from "./Header/Cart.tsx";
-import avatar from ".././assets/image-avatar.png";
 import { HeaderApp } from "../types.ts";
 
 export default function Header({
@@ -23,11 +22,20 @@ export default function Header({
           checkoutStatus={checkoutStatus}
           onDelete={onDelete}
         />
-        <img
-          src={avatar}
-          className="w-6 h-6 object-contain aspect-square hover:outline outline-1 md:outline-2 outline-orange rounded-full cursor-pointer md:w-10 md:h-10"
-          alt="avatar"
-        />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          strokeWidth="1.4px"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="w-7 h-7  md:w-8 md:h-8 object-contain rounded-full cursor-pointer  stroke-[#68707D] hover:stroke-black transition-all duration-75 ease-in-out"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+          <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+          <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+        </svg>
       </article>
     </header>
   );
