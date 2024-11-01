0c606945f7bd9dedc43b7bfe494cd61a8eb77435
Gabriel Oporto <gabyoporto@gmail.com>
2024-11-01 08:54:43 -0300

    delete no usable var colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,9 @@ export default {
   theme: {
     extend: {
       colors: {
-        orange: "hsl(26, 100%, 55%)",
-        paleOrange: "hsl(25, 100%, 94%)",
-        veryDarkBlue: "hsl(220, 13%, 13%)",
+        orange: "#001466",
         darkGrayishBlue: "hsl(219, 9%, 45%)",
         grayishBlue: "hsl(220, 14%, 75%)",
-        lightGrayishBlue: "hsl(223, 64%, 98%)",
       },
     },
   },
