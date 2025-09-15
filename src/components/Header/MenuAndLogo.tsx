7dedcb140bdac48dbf636f0cacab07f81e12c9c3
Gabriel Oporto <gabyoporto@gmail.com>
2025-09-15 19:03:26 -0300

    Refactor MenuAndLogo component to utilize UIContext for state management, removing props and enhancing code clarity

--- src/components/Header/MenuAndLogo.tsx
+++ src/components/Header/MenuAndLogo.tsx
@@ -1,20 +1,22 @@
 import logo from "../.././assets/logo.svg";
-import { MenuAndLogoApp } from "../../types";
+import { useUIContext } from "../../context/UIContext";
 
 import MenuButton from "./MenuButton";
 import Nav from "./Nav";
 
-export default function MenuAndLogo({ menu, onMenu }: MenuAndLogoApp) {
+export default function MenuAndLogo() {
+  const { menuIsActive } = useUIContext();
+
   return (
     <article className="flex gap-4 md:h-full">
-      <MenuButton onMenu={onMenu} />
+      <MenuButton />
       <img src={logo} width={138} className="h-5 md:mt-6" alt="sneakers" />
       <div
         className={`${
-          menu ? "absolute" : "hidden"
+          menuIsActive ? "absolute" : "hidden"
         } inset-0 bg-black bg-opacity-75 z-[999] min-h-screen md:hidden`}
       ></div>
-      <Nav menu={menu} onMenu={onMenu} />
+      <Nav />
     </article>
   );
 }
