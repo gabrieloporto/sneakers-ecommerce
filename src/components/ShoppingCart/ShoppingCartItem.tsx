5e613634a03c0e74b719fec630297da533ca1095
Gabriel Oporto <gabyoporto@gmail.com>
2025-09-15 19:03:56 -0300

    Refactor ShoppingCartItem component to utilize CartContext for state management, simplifying props and enhancing code clarity

--- src/components/ShoppingCart/ShoppingCartItem.tsx
+++ src/components/ShoppingCart/ShoppingCartItem.tsx
@@ -1,14 +1,12 @@
 import DetailsProduct from "./DetailsProduct.jsx";
 import QuantityItems from "./QuantityItems.jsx";
 import AddToCartButton from "./AddToCartButton.jsx";
-import { Home } from "../../types.js";
+import { useCartContext } from "../../context/CartContext";
 
-export default function ShoppingCartItem({
-  countProduct,
-  subtractProduct,
-  addProduct,
-  addItemCart,
-}: Home) {
+export default function ShoppingCartItem() {
+  const { countProduct, subtractProduct, addProduct, addItemCart } =
+    useCartContext();
+
   return (
     <section className="m-4 mb-12">
       <DetailsProduct />
