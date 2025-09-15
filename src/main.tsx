7c2a5cfe8cd2be783b089885d3239eea0388dc9d
Gabriel Oporto <gabyoporto@gmail.com>
2025-09-15 19:02:52 -0300

    Integrate CartProvider and UIProvider into main application rendering for improved state management

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { CartProvider } from "./context/CartContext.tsx";
+import { UIProvider } from "./context/UIContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <UIProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </UIProvider>
+  </React.StrictMode>
+);
