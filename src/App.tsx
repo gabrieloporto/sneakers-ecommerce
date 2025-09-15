244a521223e5bee5f7f9b17c21c5fea02fdb76ea
Gabriel Oporto <gabyoporto@gmail.com>
2025-09-15 19:02:46 -0300

    Remove unused hooks and simplify Header and ShoppingCartItem components in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import useMenu from "./hooks/useMenu.ts";
-import useCart from "./hooks/useCart.ts";
-import useCountProduct from "./hooks/useCountProduct.ts";
 import useModalState from "./hooks/useModalState.ts";
 import useResize from "./hooks/useResize.ts";
 import useSlider from "./hooks/useSlider.ts";
@@ -12,21 +9,7 @@ import MobileProductImage from "./components/ProductImage/MobileProductImage.tsx
 import DesktopProductImage from "./components/ProductImage/DesktopProductImage.tsx";
 
 export default function App() {
-  //menu
-  const { menuIsActive, handleMenu } = useMenu();
-
-  //cart
-  const { cartIsActive, handleCart } = useCart();
-  const {
-    countProduct,
-    addProduct,
-    subtractProduct,
-    addItemCart,
-    itemsInCart,
-    checkoutCart,
-    deleteItemCart,
-  } = useCountProduct();
-
+  // Hooks locales aún en uso
   const { next, previous, selectedImage } = useSlider();
   const { screenSize } = useResize();
   const { modalDesktop, setModalDesktop, handleModalDesktop } = useModalState();
@@ -36,15 +19,7 @@ export default function App() {
       {modalDesktop && (
         <ModalDesktopProduct onModalDesktop={() => setModalDesktop(false)} />
       )}
-      <Header
-        menu={menuIsActive}
-        onMenu={handleMenu}
-        onCart={handleCart}
-        cartState={cartIsActive}
-        itemsAdded={itemsInCart}
-        checkoutStatus={checkoutCart}
-        onDelete={deleteItemCart}
-      />
+      <Header />
       <main className="flex flex-col md:flex-row">
         {screenSize < 768 ? (
           <MobileProductImage
@@ -59,12 +34,7 @@ export default function App() {
         <article className="flex flex-col md:w-[40%] md:mt-4">
           <TextContent />
 
-          <ShoppingCartItem
-            countProduct={countProduct}
-            subtractProduct={subtractProduct}
-            addProduct={addProduct}
-            addItemCart={addItemCart}
-          />
+          <ShoppingCartItem />
         </article>
       </main>
     </>
